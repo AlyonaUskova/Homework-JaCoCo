78b3a046e6e3daac73022ffc3466c2486ec41f9a
Alyona Uskova <a-uskova@inbox.ru>
2021-12-08 14:04:10 +0400

    BUILD SUCCESS

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,26 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxWithoutSameValue() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {19, 5, 1, 4, 14, 3, 8, 6, 10, 11, 12};
+        long expected = 19;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxSameValue() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 2, 3};
+        long expected = 3;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
